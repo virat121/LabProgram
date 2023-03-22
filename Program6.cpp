b8994c6e95fee03dc5c280663bab546a13187a88
pacer1 <sauravvirat7156508@gmail.com>
2023-03-22 21:01:47 +0530

    Postfix evaluation

---
+++ Program6.cpp
@@ -0,0 +1,50 @@
+// Evaluation of postfix expression
+#include <iostream>
+#include <stack>
+#include <cmath>
+using namespace std;
+// only for single digit number
+int evalPostfix(string &s)
+{
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (isdigit(s[i]))
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+            int operand1 = st.top();
+            st.pop();
+            int operand2 = st.top();
+            st.pop();
+            switch (s[i])
+            {
+            case '+':
+                st.push(operand1 + operand2);
+                break;
+            case '-':
+                st.push(operand1 - operand2);
+                break;
+            case '*':
+                st.push(operand1 * operand2);
+                break;
+            case '/':
+                st.push(operand1 / operand2);
+                break;
+            case '^':
+                st.push(pow(operand2, operand1));
+            }
+        }
+    }
+    return st.top();
+}
+int main()
+{
+    string p_expr;
+    cout << "Enter the Postfix Expression: ";
+    getline(cin, p_expr);
+    cout << evalPostfix(p_expr) << endl;
+    return 0;
+}
